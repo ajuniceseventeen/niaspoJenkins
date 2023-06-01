ae93c129bc29fa98e58f222e06ebffc776c649b5
Erdem <psib1ade@mail.ru>
2023-06-01 23:08:28 +0300

    second-mvn-spring

---
+++ LabaMaven/src/main/java/com/example/LabaMaven/LabaMavenApplication.java
@@ -0,0 +1,23 @@
+package com.example.LabaMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class LabaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabaMavenApplication.class, args);
+	}
+
+}
+
+@RestController
+class Controller {
+	@GetMapping("/")
+	String hello() {
+		return "Hello!";
+	}
+}
---
+++ LabaMaven/src/test/java/com/example/LabaMaven/LabaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LabaMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
